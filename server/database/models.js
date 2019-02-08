c7959550f308d046147913b9f9136dff73e36241
David Kong <wakeman123@hotmail.com>
2019-02-08 11:29:00 -0500

    added dependencies

--- server/database/models.js
+++ server/database/models.js
@@ -1,123 +1,74 @@
-import db from "./i"
-import Sequelize from "./i"
-var faker = require('faker')
+const Sequelize = require('sequelize');
+const db = new Sequelize('sqlite:./database/Trip.db');
+let faker = require('faker');
 
-//create 2 set of promises, each an array of even and comment promises.
-
-const Event = db.define('event', {name: Sequelize.STRING})
-
-const Comment = db.define("comment", {title: Sequelize.STRING, 
-  user: Sequalize.STRING,
-  rating : Sequelize.INTEGER,
-  body: Sequelize.TEXT, 
-  // review_date: Sequelize.DATE, A time stamp is created when something is 
-  //stored into db
+const Comment = db.define('comment', {
+  title: Sequelize.STRING,
+  user: Sequelize.STRING,
+  rating: Sequelize.INTEGER,
+  body: Sequelize.TEXT,
   trip_date: Sequelize.DATE,
-  pic_URL: Sequalize.STRING,
-  mobile : Sequelize.BOOLEAN,
-  Flag : Sequelize.BOOLEAN
-})
+  pic_URL: Sequelize.STRING,
+  mobile: Sequelize.BOOLEAN,
+  Flag: Sequelize.BOOLEAN
+});
 
-Event.hasMany(Comment)
-var events = [];
-var comments = []
+const Event = db.define('events', {
+  name: Sequelize.STRING
+});
 
-// var generateComments = function(){
-for (let i = 0; i < Math.floor(Math.random()*25) +1; i++) {
-  comments.push(Comment.create({user: faker.internet.userName, 
-      rating: Math.floor(Math.random() * 5 + 1), 
-      body: faker.lorem.paragraphs, 
-      trip_date: faker.date.past, 
-      pic_URL : faker.image.city, 
-      mobile: Math.random() >= 0.5, 
-      flag : Math.random() >= 0.95}))
-}
+Event.Comments = Event.hasMany(Comment);
 
-Promise.all(comments)
-  // .then()
-  .then(console.log(comments))
-  .catch(error => {
-      console.error(error)
+let generateComments = function() {
+  let comments = [];
+
+  for (let i = 0; i < Math.floor(Math.random() * 25) + 1; i++) {
+    comments.push({
+      user: faker.internet.userName(),
+      rating: Math.floor(Math.random() * 5 + 1),
+      body: faker.lorem.paragraphs(),
+      trip_date: faker.date.past(),
+      pic_URL: faker.image.city(),
+      mobile: Math.random() >= 0.5,
+      flag: Math.random() >= 0.95
+    });
+  }
+  return comments;
+};
+
+let generateEvents = () => {
+  let events = [];
+  for (var i = 0; i < 100; i++) {
+    events.push(
+      Event.create(
+        { name: faker.address.city(), comments: generateComments() },
+        {
+          include: [Event.Comments]
+        }
+      )
+    );
+  }
+
+  return Promise.all(events);
+};
+
+let run = async () => {
+  let events = await generateEvents();
+  if (events) {
+    console.log('Worked');
+  } else {
+    console.log("Events not loaded");
+  }
+};
+
+db.sync({ force: true })
+  .then(() => {
+    run();
   })
-
-  
-
-for (var i = 0; i < 100; i++){
-
-  events.push(Event.create({name: faker.address.city,
-       comments: comments
-          }))
-  // return Event.create({title: faker.})
-      
-}
-
-Promise.all(events)
-  .then(console.log("events created"))
-  .catch(error => {
-      console.error(error)
-  })
+  .catch(console.log("no messages saved"));
 
 
 
-
-// const Comment = db.define('comment', {
-//     title: Sequelize.STRING,
-//     user: Sequalize.STRING,
-//     rating: Sequelize.INTEGER,
-//     body: Sequelize.TEXT,
-//     // review_date: Sequelize.DATE, A time stamp is created when something is
-//     //stored into db
-//     trip_date: Sequelize.DATE,
-//     pic_URL: Sequalize.STRING,
-//     mobile: Sequelize.BOOLEAN,
-//     Flag: Sequelize.BOOLEAN
-//   });
-  
-//   Event.hasMany(Comment);
-  
-//   let generateComments = function() {
-//     let comments = [];
-  
-//     for (let i = 0; i < Math.floor(Math.random() * 25) + 1; i++) {
-//       comments.push({
-//         user: faker.internet.userName,
-//         rating: Math.floor(Math.random() * 5 + 1),
-//         body: faker.lorem.paragraphs,
-//         trip_date: faker.date.past,
-//         pic_URL: faker.image.city,
-//         mobile: Math.random() >= 0.5, 
-//         flag: Math.random() >= 0.95 // biased boolean generator 
-//       });
-//     }
-//     return comments;
-//   };
-  
-//   let generateEvents = () => {
-//     let events = [];
-//     for (var i = 0; i < 100; i++) {
-//       events.push(
-//         Event.create({ name: faker.address.city, comments: generateComments() })
-//       );
-//     }
-  
-//     return Promise.all(events)
-//       .then(console.log('saved into database'))
-//       .catch((error) =>{
-//         console.log("Something went wrong!", error)
-//       })
-//   };
-  
-  // Promise.all(events)
-  //   .then(console.log('events created'))
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-  
-  // (async () => {
-  //   // let comments = await generateComments();
-  //   let events = await generateEvents();
-  // })()
-  // ;
 
 
 //  const User = db.define("user", {name : Sequelize.STRING, 
